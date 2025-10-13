7d239fb936545cdf7981219a93e3646dfdde59ad
AkeemGit <500002961@my.fiveislands.uwi.edu>
2025-10-13 15:18:09 -0400

    Added play functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,35 @@
 document.addEventListener('DOMContentLoaded', function(){
     //console.log("Content Loaded");
     let squares = document.querySelectorAll("#board div");
+    let currentPlayer = 'X';
+    let boardState = Array(9).fill(null);
 
-    squares.forEach(div =>{
-        div.classList.add('square');
+    squares.forEach((square, index) =>{
+        square.classList.add('square');
+
+        square.addEventListener('click', () => {
+            //Preventing box from changing
+            if (boardState[index] !== null) return;
+            
+            //adding play to array
+            boardState[index] = currentPlayer;
+            
+            //Adding content to box
+            square.textContent = currentPlayer;
+
+            //Updating square style based on player
+            square.classList.add(currentPlayer.toLowerCase());
+
+            //Changing player
+            if (currentPlayer === 'X'){
+                currentPlayer = 'O';
+            }else{
+                currentPlayer = 'X';
+            }
+            
+            
+            
+        });
     });
     
 
