0f1655ca0e5ca6d114be53d51b9fa439d9a8d618
AkeemGit <500002961@my.fiveislands.uwi.edu>
2025-10-13 16:59:27 -0400

    Added game winning logic

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,6 +3,46 @@ document.addEventListener('DOMContentLoaded', function(){
     let squares = document.querySelectorAll("#board div");
     let currentPlayer = 'X';
     let boardState = Array(9).fill(null);
+    
+    
+
+    let winningCombinations = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+     ];
+
+     function checkWin(player){
+        for (let combination of winningCombinations){
+            let hasWon = true;
+
+            //Checking each winning combinations against the current board
+            for (let index of combination){
+                if (boardState[index] !== player){
+                    hasWon = false;
+                    break;
+                }
+            }
+            //A winning combination has been found.
+            if (hasWon){
+                return true;
+            }
+        }
+        //no winning combination found
+        return false;
+     }
+
+
+    function checkDraw(){
+        return boardState.every(square => square !== null);
+     }
+
+
 
     squares.forEach((square, index) =>{
         square.classList.add('square');
@@ -27,6 +67,22 @@ document.addEventListener('DOMContentLoaded', function(){
 
             //Updating square style based on player
             square.classList.add(currentPlayer.toLowerCase());
+
+            
+
+            if (checkWin(currentPlayer)){
+                
+                let message = document.querySelector("#status");
+                message.classList.add('you-won');
+                message.innerHTML = `Congratulations! ${currentPlayer} is the Winner!`;
+                //console.log("Won");
+            }
+
+            if (checkDraw()){
+                
+                let message = document.querySelector("#status");
+                message.innerHTML = `Congratulations! ${currentPlayer} is the Winner!`;
+            }
 
             //Changing player
             if (currentPlayer === 'X'){
